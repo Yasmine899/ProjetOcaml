560f9ea6d3d0cc44f412aa3ae0a95a561d1073a3
Yasmine <graneyasmine@gmail.com>
2024-01-12 21:32:52 +0100

    esperons le medium

--- ocaml-maxflow-project-master/src/bipartite.mli
+++
@@ -1,7 +0,0 @@
-(* BipartiteMatching.mli *)
-open Graph
-
- val bipartite_matching : int -> int -> int list list -> (id * id) list
-val create_bipartite_graph :int -> int -> int list list -> int graph
-
-
